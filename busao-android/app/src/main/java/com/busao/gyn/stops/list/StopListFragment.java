61291ba00fa560033658ed32d78cfa02cbc327c7
Cezar Carneiro <cezargcarneiro@gmail.com>
2017-08-15 15:14:08 -0300

    improve favorite stops list

--- busao-android/app/src/main/java/com/busao/gyn/stops/list/StopListFragment.java
+++ busao-android/app/src/main/java/com/busao/gyn/stops/list/StopListFragment.java
@@ -9,9 +9,11 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.ViewSwitcher;
 
 import com.busao.gyn.R;
+import com.busao.gyn.components.RecyclerViewEmptySupport;
 import com.busao.gyn.data.BusStopDataSource;
 import com.busao.gyn.events.BusStopChanged;
 import com.busao.gyn.stops.BusStop;
@@ -26,8 +28,6 @@ import java.util.List;
  */
 public class StopListFragment extends Fragment {
 
-    private ViewSwitcher mStopListViewSwitcher;
-
     private StopsRecyclerViewAdapter mAdapter;
 
     private BusStopDataSource mDataSource;
@@ -37,22 +37,20 @@ public class StopListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.bus_stops_list_fragment, null);
 
-        mStopListViewSwitcher = (ViewSwitcher) view.findViewById(R.id.stopListViewSwitcher);
-        RecyclerView stopsRecyclerView = (RecyclerView) view.findViewById(R.id.stopListRecyclerView);
+        TextView stopListNoFavoritesTextView = (TextView) view.findViewById(R.id.stopListNoFavoritesTextView);
+        RecyclerViewEmptySupport stopsRecyclerView = (RecyclerViewEmptySupport) view.findViewById(R.id.stopListRecyclerView);
+        stopsRecyclerView.setmEmptyView(stopListNoFavoritesTextView);
 
         // use this setting to improve performance if you know that changes
         // in content do not change the layout size of the RecyclerView
         stopsRecyclerView.setHasFixedSize(false);
 
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
-        stopsRecyclerView.setLayoutManager(layoutManager);
+        stopsRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         stopsRecyclerView.setItemAnimator(new DefaultItemAnimator());
 
         mDataSource = BusStopDataSource.newInstance(getContext());
-        List<BusStop> stops = mDataSource.read();
-        mAdapter = new StopsRecyclerViewAdapter(mDataSource, stops);
+        mAdapter = new StopsRecyclerViewAdapter(mDataSource);
         stopsRecyclerView.setAdapter(mAdapter);
-        switchViews(stops);
         return view;
     }
 
@@ -66,6 +64,7 @@ public class StopListFragment extends Fragment {
     public void onStart(){
         super.onStart();
         EventBus.getDefault().register(this);
+        mAdapter.refresh(mDataSource.read());
     }
 
     @Override
@@ -79,13 +78,4 @@ public class StopListFragment extends Fragment {
         mAdapter.refresh(mDataSource.read());
     }
 
-    private void switchViews(List<BusStop> stops) {
-        if (stops != null && stops.size() > 0) {
-            if (R.id.stopListRecyclerView == mStopListViewSwitcher.getNextView().getId()) {
-                mStopListViewSwitcher.showNext();
-            }
-        } else if (R.id.noItemsTextView == mStopListViewSwitcher.getNextView().getId()) {
-            mStopListViewSwitcher.showNext();
-        }
-    }
 }
